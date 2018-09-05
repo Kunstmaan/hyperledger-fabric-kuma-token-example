3dc4599089ebf752e1d5af05dfa3635b46ab676d
GLeurquin <GLeurquin@users.noreply.github.com>
2018-09-05 13:15:35 +0200

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+# Exit immediately if a pipeline returns non-zero status.
+# Result of pipeline is the last command
+# Unset variables are considered an error
+set -e -o pipefail
+USER=$(whoami)
+
+# Remove interraction
+export DEBIAN_FRONTEND=noninteractive
+HYPERLEDGER_VERSION='1.1.0'
+HYPERLEDGER_BASE_VERSION='0.4.6'
+COUCHDB_VERSION='0.4.10'
+NODEJS_VERSION='8.9.4'
+DOCKER_COMPOSE_VERSION='1.19.0'
+
+puts() {
+    local GREEN='\033[0;32m'
+    local NC='\033[0m'
+    >&2 echo -e "${GREEN}$*${NC}"
+}
+
+err() {
+  local RED='\033[0;31m'
+  local NC='\033[0m'
+  >&2 echo -e "${RED}$*${NC}"
+}
+
+warn() {
+  local YELLOW='\033[0;33m'
+  local NC='\033[0m'
+  >&2 echo -e "${YELLOW}$*${NC}"
+}
+
+abort() {
+    err "Aborting..."
+    exit 1
+}
+
+usage() {
+    err "Usage: $(basename "$0")"
+    abort
+}
+
+if [ "$#" -lt 0 ]; then
+    err "Illegal number of parameters"
+    usage
+    abort
+fi
+
+if [ "$(uname)" != 'Linux' ]; then
+    err "Not running on linux"
+    abort
+else
+    if ! groups | grep -q "root"; then
+        err "Must run as root."
+        abort
+    fi
+fi
+
+apt-get update
+
+if command -v wget > /dev/null 2>&1; then puts "wget is already installed. Skipping..."; else {
+  puts "Installing wget..."
+  apt-get install -y wget
+  puts "Done."
+}; fi
+
+# Install docker
+if command -v docker > /dev/null 2>&1; then puts "Docker is already installed. Skipping..."; else {
+  puts "Installing Docker..."
+
+  apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+  add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+  apt-get update
+
+  apt-get install -y docker-ce
+  usermod -a -G docker $USER
+  service docker start
+  puts "Done."
+}; fi
+
+if command -v docker-compose > /dev/null 2>&1; then puts "Docker-compose is already installed. Skipping..."; else {
+    puts "Installing Docker-compose..."
+    curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo chmod 755 /usr/local/bin/docker-compose
+    puts "Done."
+}; fi
+
+if command -v node > /dev/null 2>&1; then puts "NodeJs is already installed. Skipping..."; else {
+    puts "Installing NodeJs..."
+    NODE_FILE="node-v$NODEJS_VERSION-linux-x64.tar.gz"
+    wget http://nodejs.org/dist/v$NODEJS_VERSION/$NODE_FILE
+    tar --strip-components 1 -xzvf $NODE_FILE -C /usr/local
+    ln -s /usr/local/bin/node /usr/bin/node
+    ln -s /usr/local/bin/npm /usr/bin/npm
+    puts "Done."
+}; fi
+
+if command -v git > /dev/null 2>&1; then puts "Git is already installed. Skipping..."; else {
+    puts "Installing Git..."
+    apt-get install -y git
+    puts "Done."
+}; fi
+
+if command -v python > /dev/null 2>&1; then puts "Python is already installed. Skipping..."; else {
+    puts "Installing Python..."
+    apt-get install -y python
+    puts "Done."
+}; fi
+
+if command -v make > /dev/null 2>&1; then puts "Make is already installed. Skipping..."; else {
+    puts "Installing Make..."
+    apt-get install -y make
+    puts "Done."
+}; fi
+
+if command -v g++ > /dev/null 2>&1; then puts "G++ is already installed. Skipping..."; else {
+    puts "Installing G++..."
+    apt-get install -y g++
+    puts "Done."
+}; fi
+
+if ! groups | grep -q docker; then
+    puts "Please restart the console and run the script again to allow the user group modifications to take place."
+    exit 0
+fi
+
+puts "Pulling docker images..."
+docker pull "hyperledger/fabric-couchdb:$COUCHDB_VERSION" &
+docker pull "hyperledger/fabric-ccenv:x86_64-$HYPERLEDGER_VERSION" &
+docker pull "hyperledger/fabric-peer:x86_64-$HYPERLEDGER_VERSION" &
+docker pull "hyperledger/fabric-orderer:x86_64-$HYPERLEDGER_VERSION" &
+docker pull "hyperledger/fabric-tools:x86_64-$HYPERLEDGER_VERSION" &
+docker pull "hyperledger/fabric-ccenv:x86_64-$HYPERLEDGER_VERSION" &
+docker pull "hyperledger/fabric-baseimage:x86_64-$HYPERLEDGER_BASE_VERSION" &
+wait
+
+# normal_user="$1"
+#
+#
+# sudo -u $normal_user bash << EOF
+# REPO="hyperleder-fabric-kuma-token-example"
+# # Setup the training example
+# if [ ! -d "$REPO" ]; then
+#     git clone https://github.com/GLeurquin/$REPO.git
+# fi
+# cd $REPO
+# npm install --prefix client
+# npm install --prefix chaincode
+# EOF
